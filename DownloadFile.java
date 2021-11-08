0501e8744ac52d3f5bdc1f0bae16d85e6b0c4734
Shakileash5 <shakileash2000@gmail.com>
2021-11-08 17:23:47 +0530

    Feat: Implementing Resumable download. Need bug fix.

--- DownloadFile.java
+++ DownloadFile.java
@@ -5,9 +5,38 @@ import java.io.*;
 
 public class DownloadFile {
     
+    public static boolean downloadFile(int fileSize, BufferedInputStream bis, BufferedOutputStream bos) throws IOException {
+        byte[] data = new byte[fileSize];
+        int bytesRead = 0;
+        int bytesFrom = 0;
+        int toBeRead = 1024;
+        int bytesWritten = 0;
+        int toBeWritten = 1024;
+
+        while(bis.available() > 0){
+            // read from the input stream
+            if(toBeRead > bis.available()){
+                toBeRead = bis.available();
+            }
+            System.out.println("Reading: " + bytesFrom + " till: " + toBeRead + " Available: "+ bis.available());
+            bytesRead = bis.read(data,bytesFrom,toBeRead);
+            bytesFrom += bytesRead;
+
+            // write into the file
+            if(toBeWritten > fileSize - bytesWritten){
+                toBeWritten = fileSize - bytesWritten;
+            }
+            bos.write(data,bytesWritten,toBeWritten);
+            bytesWritten += toBeWritten;
+            break;
+
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
         String url = "http://www.google.com/images/srpr/logo3w.png";
-        String fileName = "logo";
+        //String fileName = "logo";
         String fileNameUrl = url.substring( url.lastIndexOf('/')+1, url.length() );
         // remove extension if there is one
         fileNameUrl = fileNameUrl.substring( 0, fileNameUrl.lastIndexOf('.') );
@@ -27,7 +56,7 @@ public class DownloadFile {
             // get the file extension
             extension = conn.getHeaderField("content-type"); //  store the mime type
             extension = MimeTypes.getDefaultExt(extension); // convert the mime type into extention
-            fileName += "." + extension; // add extention to the file name
+            //fileName += "." + extension; // add extention to the file name
             fileNameUrl += "." + extension; // add extention to the file name
             System.out.println("Extension: " + extension);
 
@@ -39,42 +68,19 @@ public class DownloadFile {
             System.out.println("File Name: " + fileNameUrl);
             if(Files.exists(Paths.get(fileNameUrl))){
                 System.out.println("File already exists");
-                //flagFileExist = 1;
+                flagFileExist = 1;
                 //System.exit(0);
             }
 
-            FileOutputStream fos = new FileOutputStream(fileNameUrl);
-            BufferedOutputStream bos = new BufferedOutputStream(fos);
-
-            InputStream is = conn.getInputStream();
-            BufferedInputStream bis = new BufferedInputStream(is);
+            
 
             // download the file
             if(flagFileExist == 0){
-                byte[] data = new byte[fileSize];
-                int bytesRead = 0;
-                int bytesFrom = 0;
-                int toBeRead = 1024;
-                int bytesWritten = 0;
-                int toBeWritten = 1024;
-
-                while(bis.available() > 0){
-                    // read from the input stream
-                    if(toBeRead > bis.available()){
-                        toBeRead = bis.available();
-                    }
-                    System.out.println("Reading: " + bytesFrom + " till: " + toBeRead + " Available: "+ bis.available());
-                    bytesRead = bis.read(data,bytesFrom,toBeRead);
-                    bytesFrom += bytesRead;
-
-                    // write into the file
-                    if(toBeWritten > fileSize - bytesWritten){
-                        toBeWritten = fileSize - bytesWritten;
-                    }
-                    bos.write(data,bytesWritten,toBeWritten);
-                    bytesWritten += toBeWritten;
-
-                }
+                FileOutputStream fos = new FileOutputStream(fileNameUrl);
+                BufferedOutputStream bos = new BufferedOutputStream(fos);
+                InputStream is = conn.getInputStream();
+                BufferedInputStream bis = new BufferedInputStream(is);
+                downloadFile(fileSize,bis,bos);
 
                 bos.close();
                 bis.close();
@@ -82,13 +88,27 @@ public class DownloadFile {
                 fos.close();
             }
             else{
-                System.out.println("File already exists");
+                FileOutputStream fos = new FileOutputStream(fileNameUrl,true);
+                BufferedOutputStream bos = new BufferedOutputStream(fos);
+                long existingFileSize = fos.getChannel().size();
+                System.out.println("Existing File Size: " + existingFileSize);
+                if(existingFileSize < fileSize){
+                    conn.setRequestProperty("Range", "bytes=" + existingFileSize + "-" + fileSize);
+                    InputStream is = conn.getInputStream();
+                    BufferedInputStream bis = new BufferedInputStream(is);
+                    downloadFile(fileSize,bis,bos);
+                    bos.close();
+                bis.close();
+                
+                }
+                
+                fos.close();
             }
             
 
         }
         catch(Exception e) {
-            System.out.println("Error: " + e);
+            System.out.println("Error: " + e + e.getStackTrace());
         }
     }
 
