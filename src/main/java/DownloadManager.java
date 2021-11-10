5e9005b4435131db33d85e5b87110b4a0d2216ec
Shakileash5 <shakileash2000@gmail.com>
2021-11-10 20:32:50 +0530

    Feat: created unique filename with hash

--- src/main/java/DownloadManager.java
+++ src/main/java/DownloadManager.java
@@ -2,6 +2,9 @@ package src.main.java;
 
 import java.io.*;
 import java.net.*;
+import java.security.*;
+import java.util.Arrays;
+
 import src.main.java.*;
 
 /*
@@ -30,21 +33,56 @@ public class DownloadManager {
         
     }
 
+    /*
+     * This method is responsible for creating short hash the URL.
+     * the short hash is used to create a unique file name, if file name is not present in the url.
+     * 
+     * @param: url - the URL of the file to be downloaded.
+     * @return: hash - the short hash of the URL.
+     */
+    private String getHash(String url){
+        char[] characters=url.toCharArray();
+        Arrays.sort(characters);
+        return Integer.toString(new String(characters).hashCode());
+    }
+
+    /*
+     * This method is responsible for finding and creating extension of the file
+     * and also creating the file name.
+     * 
+     * @param: url - the URL of the file to be downloaded.
+     * @param: mimeType - the mime type of the file.
+     * @return: None
+     */
     private void setFileData(String url, String mimeType){
         String extentionFromUrl = new String();
 
+        // Get the file name from the URL
+        this.fileName = url.substring( url.lastIndexOf('/')+1, url.length() );
+        if(this.fileName.lastIndexOf('.') != -1){
+            extentionFromUrl = this.fileName.substring(this.fileName.lastIndexOf('.'), this.fileName.length()); // get the extention from the URL
+            this.fileName = this.fileName.substring(0, this.fileName.lastIndexOf('.')); // remove the extension if any
+        }
+        System.out.println("extentio from url: "+extentionFromUrl);
+        if(extentionFromUrl.length()<0 || extentionFromUrl.equals("")){
+            System.out.println("No extention found");
+            this.fileName = "download_" + this.getHash(url); // if no extention found, use the hash of the url as the file name
+        }
+
         // clean mimeType
         if(mimeType.contains(";")){
-            mimeType = mimeType.substring(0, mimeType.indexOf(";"));
+            this.mimeType = mimeType.substring(0, mimeType.indexOf(";"));
         }
-         // Get the file name from the URL
-         this.fileName = url.substring( url.lastIndexOf('/')+1, url.length() );
-         if(this.fileName.lastIndexOf('.') != -1){
-             extentionFromUrl = this.fileName.substring(this.fileName.lastIndexOf('.'), this.fileName.length()); // get the extention from the URL
-             this.fileName = this.fileName.substring(0, this.fileName.lastIndexOf('.')); // remove the extension if any
-         }
+        System.out.println("mimeType: "+mimeType+"mime: ");
+        // set extention
+        this.extention = MimeTypes.getDefaultExt(this.mimeType);
+        if(extentionFromUrl.length() > 0 && (this.extention.length() == 0 || this.extention.equals("unknown"))){
+            this.extention = extentionFromUrl;
+        }
+        System.out.println("File name: " + this.fileName + this.extention + "Mime: " + this.mimeType);
+        this.fileName = this.fileName + "." + this.extention;
     }
-
+    
     /*
      * This method is responsible for handling the metadata of the file.
      * 
@@ -57,27 +95,8 @@ public class DownloadManager {
             URLConnection conn = urlObj.openConnection();
             this.mimeType = conn.getContentType();
             this.fileSize = conn.getContentLength();
-            String extentionFromUrl = new String();
-
-            // Get the file name from the URL
-            this.fileName = url.substring( url.lastIndexOf('/')+1, url.length() );
-            if(this.fileName.lastIndexOf('.') != -1){
-                extentionFromUrl = this.fileName.substring(this.fileName.lastIndexOf('.'), this.fileName.length()); // get the extention from the URL
-                this.fileName = this.fileName.substring(0, this.fileName.lastIndexOf('.')); // remove the extension if any
-            }
-
-            // clean mimeType
-            if(mimeType.contains(";")){
-                mimeType = mimeType.substring(0, mimeType.indexOf(";"));
-            }
-            // set extention
-            this.extention = MimeTypes.getDefaultExt(this.mimeType);
-            if(extentionFromUrl.length() > 0 && (this.extention.length() == 0 || this.extention.equals("unknown"))){
-                this.extention = extentionFromUrl;
-            }
-            System.out.println("File name: " + this.fileName + this.extention + "Mime: " + this.mimeType);
-            this.fileName = this.fileName + "." + this.extention;
             
+            this.setFileData(url, mimeType);
         }
         catch(IOException e){
             System.out.println("IOException: " + e.getMessage());
